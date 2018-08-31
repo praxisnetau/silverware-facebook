49144b297772f12279daf6786457a26904818fb9
Colin Tucker <colin@praxis.net.au>
2018-08-31 18:01:49 +1000

    Updated Facebook API version (fixes console bug).

--- client/src/boot/index.js
+++ client/src/boot/index.js
@@ -9,14 +9,15 @@ $(function() {
   
   $.ajaxSetup({ cache: true });
   
-  $.getScript('//connect.facebook.net/en_US/sdk.js', function() {
+  $.getScript('https://connect.facebook.net/en_US/sdk.js', function() {
     
     var appId = $('body').attr('data-facebook-app-id');
     
     FB.init({
       appId: appId,
       xfbml: true,
-      version: 'v2.9'
+      version: 'v3.1',
+      autoLogAppEvents: true
     });
     
     $('#loginbutton, #feedbutton').removeAttr('disabled');
